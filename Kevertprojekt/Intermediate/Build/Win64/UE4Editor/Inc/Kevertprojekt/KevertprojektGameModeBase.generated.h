87a771cd1396de256f86882ec93e05ed2c3cb882
DESKTOP-N6K1GOL\szapa <szapa@windowslive.com>
2017-12-29 13:31:30 +0100

    Blank projekt

---
+++ Kevertprojekt/Intermediate/Build/Win64/UE4Editor/Inc/Kevertprojekt/KevertprojektGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef KEVERTPROJEKT_KevertprojektGameModeBase_generated_h
+#error "KevertprojektGameModeBase.generated.h already included, missing '#pragma once' in KevertprojektGameModeBase.h"
+#endif
+#define KEVERTPROJEKT_KevertprojektGameModeBase_generated_h
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_RPC_WRAPPERS
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAKevertprojektGameModeBase(); \
+	friend KEVERTPROJEKT_API class UClass* Z_Construct_UClass_AKevertprojektGameModeBase(); \
+public: \
+	DECLARE_CLASS(AKevertprojektGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Kevertprojekt"), NO_API) \
+	DECLARE_SERIALIZER(AKevertprojektGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAKevertprojektGameModeBase(); \
+	friend KEVERTPROJEKT_API class UClass* Z_Construct_UClass_AKevertprojektGameModeBase(); \
+public: \
+	DECLARE_CLASS(AKevertprojektGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Kevertprojekt"), NO_API) \
+	DECLARE_SERIALIZER(AKevertprojektGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AKevertprojektGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AKevertprojektGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AKevertprojektGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AKevertprojektGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AKevertprojektGameModeBase(AKevertprojektGameModeBase&&); \
+	NO_API AKevertprojektGameModeBase(const AKevertprojektGameModeBase&); \
+public:
+
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AKevertprojektGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AKevertprojektGameModeBase(AKevertprojektGameModeBase&&); \
+	NO_API AKevertprojektGameModeBase(const AKevertprojektGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AKevertprojektGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AKevertprojektGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AKevertprojektGameModeBase)
+
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_12_PROLOG
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_RPC_WRAPPERS \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_INCLASS \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Kevertprojekt_Source_Kevertprojekt_KevertprojektGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
