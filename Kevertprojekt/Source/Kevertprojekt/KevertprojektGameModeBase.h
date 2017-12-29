6905b9b5b9b0c7beebf1c621448ce53f281dfd5c
DESKTOP-N6K1GOL\szapa <szapa@windowslive.com>
2017-12-29 13:31:30 +0100

    Blank projekt

---
+++ Kevertprojekt/Source/Kevertprojekt/KevertprojektGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KevertprojektGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEVERTPROJEKT_API AKevertprojektGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
